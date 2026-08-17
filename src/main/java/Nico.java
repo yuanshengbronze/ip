56a7efb437e12650cdbc417eb951fdfae90cd7e2
elbert <elbert.tristanlie1@gmail.com>
2026-08-17 13:34:46 +0800

    Level 1. Chatbot echoes user's input, and exits with command "bye"

--- src/main/java/Nico.java
+++ src/main/java/Nico.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Nico chatbot application.
  */
 public class Nico {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String LINE = "____________________________________________________________";
         String banner =
                 "███╗   ██╗██╗ ██████╗ ██████╗ \n" +
@@ -13,9 +16,19 @@ public class Nico {
                 "╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ \n";
         System.out.println(LINE);
         System.out.println(banner);
-        System.out.println("Hey man! It's Nico, what can I do for you?");
-        System.out.println(LINE);
-        System.out.println("Nice seeing you. Until next time!");
-        System.out.println(LINE);
+        System.out.println("     Hey man! It's Nico, what can I do for you?");
+        while(true) {
+            System.out.println("     " + LINE);
+            String ans = scanner.nextLine();
+            if(ans.equals("bye")) {
+                System.out.println("     " + LINE);
+                System.out.println("     Nice seeing you. Until next time!");
+                System.out.println("     " + LINE);
+                break;
+            } else {
+                System.out.println("     " + LINE);
+                System.out.println("     " + ans);
+            }
+        }
     }
 }
